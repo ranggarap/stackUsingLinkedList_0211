cffd499db1dd85a11731c95cf8acdc14fdbc6e39
ranggarap <fadhilahrangga26@gmail.com>
2025-05-20 10:38:19 +0700

    if else and while

--- stack.cpp
+++ stack.cpp
@@ -43,4 +43,21 @@ void pop()
     cout <<" popped Value:"<< top ->data << endl;
     top = top ->next;
 }
+void peek()
+{
+    if (top == NULL)
+    {
+        cout << "List is Empty." << endl;
+    }
+    else
+    {
+        Node *current =top;
+        while (current != NULL)
+        {
+            cout << current -> data<< " " <<endl;
+            current =current -> next ;
+        }
+        cout <<endl;
+    }
+}
 };
